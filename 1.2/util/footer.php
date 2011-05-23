06ef5a8cbdbfdb4f053fe7834f8413039f7b9305
derek.leung12 <derek.leung12@a17a41c2-2b56-6b4f-addd-ff604170e165>
2011-05-23 05:58:44 +0000

    dleung: added disclaimer, change as needed
    git-svn-id: https://svn.kenai.com/svn/bellbook~bellbook_source/branch@27 a17a41c2-2b56-6b4f-addd-ff604170e165

--- 1.2/util/footer.php
+++ 1.2/util/footer.php
@@ -2,6 +2,10 @@
 <!--//page content-->
 <!--footer-->
 <hr />
+<div> <small> DISCLAIMER: Bellarmine is hosting this service solely as
+        a convenience for students and parents. Bellarmine is not responsible
+        in any way for the price, edition, or quality of books posted by 
+        students to this site. </small> </div>
 <div> <small> If you need help or wish to contribute to bellbook, contact
         <a href="mailto:derek.leung12@bcp.org"> Derek Leung</a> </small> </div>
 <!--//footer-->
