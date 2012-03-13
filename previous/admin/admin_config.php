450c3115bcf75df3a1d29da083b772868c6fd2d2
Ben Chan <vervious@gmail.com>
2012-03-12 21:23:36 -0700

    Not sure what this conflict is…

--- previous/admin/admin_config.php
+++ previous/admin/admin_config.php
@@ -1,7 +1,13 @@
 <?php
+<<<<<<< Updated upstream:previous/admin/admin_config.php
 $USER='derek.leung12.admin';
 $PASSWORD='GreenSubsidy_20percent';
 $DATABASE='12_bellbook';
+=======
+$USER='root';
+$PASSWORD='';
+$DATABASE='bellbook';
+>>>>>>> Stashed changes:util/admin_config.php
 $ADDRESS='localhost';
 $EDIT='dontkillthefrogs';
 ?>
