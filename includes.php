cd2d6985e4f8ccbf60a7866906abaa775db55673
Derek Leung <dltnf.alt@gmail.com>
2012-05-14 23:22:14 -0700

    test commit

---
+++ includes.php
@@ -0,0 +1,4 @@
+<?php
+function sanitize() {
+    foreach ($_GET as $key => $value) {
+        
---
+++ index.php
@@ -0,0 +1,9 @@
+<? require_once('includes.php'); ?>
+<? require_once('header.php'); ?>
+<? content($query); ?>
+
+<!--
+Project Bellbook (version Cascade)
+Derek Leung
+Bellarmine College Preparatory, 2012
+-->
