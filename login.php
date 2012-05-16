b0770cf51f537404f467a20d3d46d3532cab6cd9
Derek Leung <dltnf.alt@gmail.com>
2012-05-15 23:42:45 -0700

    added login page

---
+++ login.php
@@ -0,0 +1,6 @@
+<p>Log in with your BCP account: </p>
+<form action='dummy.php' method='post'>
+Username: <input type='text' name='username' /> <br />
+Password: <input type='password' name='password' />
+<input type='submit' />
+</form>
