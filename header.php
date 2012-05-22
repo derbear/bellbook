2bf6ee4813c5dc4d65fb5b80d9ded52d00dd1fb2
Derek Leung <dltnf.alt@gmail.com>
2012-05-22 15:48:01 -0700

    cleaning

--- header.php
+++
@@ -1,7 +0,0 @@
-<div align='right'> navigation:
-	<? listLink('home', 'home'); ?> |
-	<? listLink('login', 'login'); ?>
-</div>
-<div>
-	<font color='green' id="message"> </font>
-</div>
--- login.php
+++
@@ -1,9 +0,0 @@
-<p>Log in with your BCP account: </p>
-<? require_once('util.php'); 
-$params = array(
-	'names' => array('username' => 'text', 'password' => 'password'),
-	'target' => 'dummy.php', 
-	'method' => 'post.php',
-	'button' => 'Log in');
-makeForm($params);
-?>
