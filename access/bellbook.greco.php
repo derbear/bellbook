6ffaa2ac2f464516dd736b0e5dd520d88dfea000
Derek Leung <dltnf.alt@gmail.com>
2012-05-23 15:45:15 -0700

    fixed problem with authentication

--- access/bellbook.greco.php
+++ access/bellbook.greco.php
@@ -6,6 +6,14 @@
  * This ONLY WORKS ON GRECO directly - set up some way to grab info as testing?
  */
 function auth($bcpName, $password) {
+	$host       = 'ldap.bcp.org';
+	$port       = 389;
+	$baseDn 	= 'OU=Vista,DC=synergy,DC=bcp,DC=org';
+	$user       = 'CN=ldap_webaccounts,CN=users,DC=synergy,DC=bcp,DC=org';
+	$pass       = '12345';
+
+	$ds = ldap_connect($host, $port);
+
 	$result = userinfo($bcpName);
 	$ds = ldap_connect($host, $port);
 	if($result[0]) {
