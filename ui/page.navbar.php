0ee7bf1c1fea0daab7dbb6300d9a57395b8bae32
Derek Leung <dltnf.alt@gmail.com>
2012-05-24 14:01:10 -0700

    navigation bar now displays passed-in messages

--- ui/page.navbar.php
+++ ui/page.navbar.php
@@ -2,6 +2,8 @@
 <a href='index.php?query=home'>Home</a> | 
 <a href='index.php?query=status'>Status</a> |
 <a href='index.php?query=help'>Help</a> |
+<a href='index.php?query=login'>Login</a> |
 <a href='index.php?query=about'>About</a>
+<? if(isset($_GET['message'])) echo '<div> ' . $_GET['message'] . ' </div>'; ?>
 <hr />
 <? // loop through titles? do we need another meta-variable? ?>
