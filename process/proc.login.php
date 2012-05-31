0c3562bd2ef154a16fade2ea9450c1feb398ab83
Derek Leung <dltnf.alt@gmail.com>
2012-05-31 13:30:03 -0700

    first-time logins are recorded in database now

--- process/proc.login.php
+++ process/proc.login.php
@@ -1,6 +1,7 @@
 <?php
 include('../access/bellbook.includes.php');
 sanitize();
+connect();
 if(isset($_GET['logout']) && $_GET['logout']) {
 	session_start();
 	session_destroy();
@@ -17,7 +18,27 @@ if(isset($_GET['logout']) && $_GET['logout']) {
 		$_SESSION['id'] = getId($info);
 		$_SESSION['given'] = getGiven($info);
 		$_SESSION['cn'] = getCn($info);
-		header('Location: ../index.php?query=status&message=Welcome, ' . $_SESSION['given']);
+		$id = $_SESSION['id'];
+		
+		$query = "SELECT * FROM Users WHERE id=$id";
+		$resource=mysql_query($query);
+		if(!$resource) {
+			session_destroy();
+			header('Location: ../index.php?query=login&message=There was a database error; please try again');
+		} else {
+			$row=mysql_fetch_row($resource);
+			if($row) { // user is in table already
+				header('Location: ../index.php?query=status&message=Welcome, ' . $_SESSION['given']);
+			} else { // attempt to put user ID into table
+				$query = "INSERT INTO Users VALUES ($id)";
+				if(mysql_query($query)) {
+					header('Location: ../index.php?query=status&message=Welcome, ' . $_SESSION['given']);
+				} else {
+					session_destroy();
+					header('Location: ../index.php?query=login&message=There was a database error; please try again');
+				}
+			}
+		}
 	} else {
 		header('Location: ../index.php?query=login&message=The entered username or password is invalid');
 	}
